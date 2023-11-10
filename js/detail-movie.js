a3830b42eee4f614a6e6c11363116560c34afc28
abrilreggii <areggiardo@udesa.edu.ar>
2023-11-10 17:54:08 -0300

    Hice todo el detail-movie js

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -2,20 +2,26 @@ let APIKey='7bbfa155b881d065cf760bebe36c4d28'
 
 let queryString=location.search
 let queryStringObj= new URLSearchParams(queryString)
-let nameABuscar=queryStringObj.get('busqueda')
-let busqueda= queryStringObj.get('busqueda') //recupero el queryString, o la entrada del usuario.
+let id_pelicula= queryStringObj.get('id') //recupero el queryString, o la entrada del usuario.
 
-let urlPelis=`https://api.themoviedb.org/3/search/movie?query=${busqueda}&api_key=${APIKey}`
-
+let urlPelis=`https://api.themoviedb.org/3/movie/${id_pelicula}?api_key=${APIKey}`
 
 fetch(urlPelis)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data.results); 
-        
-        
+        console.log(data); 
+        let lista_generos = []
+        for(let i=0; i<data.genres.length; i++){
+            lista_generos += data.genres[i].name + "  "
+        }
+        let section_izquierda = document.querySelector('.detalle_pelicula_izquierda')
+        let section_derecha = document.querySelector('.detalle_pelicula_derecha')
+        section_izquierda.innerHTML += `<img src="https://image.tmdb.org/t/p/w500/${data.backdrop_path}">`
+        section_derecha.innerHTML += `<article> <h1>${data.title}</h1> <p>Sinopsis: ${data.overview}</p> 
+        <p> Rating: ${data.vote_average}</p> <p> Fecha de estreno: ${data.release_date}</p> 
+        <p> Duración xmins: ${data.runtime}</p> <p> Generos: ${lista_generos}</p></article>`
     })
     .catch(function(error){
         console.log(error);
--- js/search-results.js
+++ js/search-results.js
@@ -2,7 +2,6 @@ let APIKey='7bbfa155b881d065cf760bebe36c4d28'
 
 let queryString=location.search
 let queryStringObj= new URLSearchParams(queryString)
-let nameABuscar=queryStringObj.get('busqueda')
 let busqueda= queryStringObj.get('busqueda') //recupero el queryString, o la entrada del usuario.
 
 let urlPelis=`https://api.themoviedb.org/3/search/movie?query=${busqueda}&api_key=${APIKey}`
@@ -18,7 +17,7 @@ fetch(urlPelis)
         let lista_peliculas = data.results
         for(let i = 0; i <lista_peliculas.length; i++){
             let article=document.querySelector('.peliculas')
-            article.innerHTML+=`<a href="detail-movie.html?id=${lista_peliculas[i].id}"></a><article><img src="https://image.tmdb.org/t/p/w342${lista_peliculas[i].poster_path}"><p>${lista_peliculas[i].title}</p><p>${lista_peliculas[i].release_date
+            article.innerHTML+=`<a href="detail-movie.html?id=${lista_peliculas[i].id}"><article><img src="https://image.tmdb.org/t/p/w342${lista_peliculas[i].poster_path}"><p>${lista_peliculas[i].title}</p><p>${lista_peliculas[i].release_date
         }</p></article></a>`  
         }
     })
