062c53589211fa08bdb052b3fc3906bffd2f46a5
marcosbeccar <beccarvarelam@udesa.edu.ar>
2023-11-10 15:41:08 -0300

    avance tremendo, ya entendimos

--- js/search-results.js
+++ js/search-results.js
@@ -1,11 +1,11 @@
-let APIKey='?api_key=7bbfa155b881d065cf760bebe36c4d28'
+let APIKey='7bbfa155b881d065cf760bebe36c4d28'
 
 let queryString=location.search
 let queryStringObj= new URLSearchParams(queryString)
 let nameABuscar=queryStringObj.get('busqueda')
-console.log(queryStringObj.get('busqueda')) //recupero el queryString, o la entrada del usuario.
+let busqueda= queryStringObj.get('busqueda') //recupero el queryString, o la entrada del usuario.
 
-let urlPelis=`https://api.themoviedb.org/3/movie/popular${APIKey}?title=Mission`
+let urlPelis=`https://api.themoviedb.org/3/search/movie?query=${busqueda}&api_key=${APIKey}`
 
 
 fetch(urlPelis)
@@ -13,8 +13,18 @@ fetch(urlPelis)
         return response.json();
     })
     .then(function(data){
-        console.log(data.results);      
+        console.log(data.results); 
+        let lista_peliculas=[]
+        for(let i = 0; i <5; i++){
+            let titulo = data.results[i].title;
+            lista_peliculas.push(titulo)  
+        }
+        for(let i = 0; i <lista_peliculas.length; i++){
+            let article=document.querySelector('.peliculas')
+            article.innerHTML+=`<article><p>${lista_peliculas[i]}</p></article>`  
+        }
     })
     .catch(function(error){
         console.log(error);
     })
+
